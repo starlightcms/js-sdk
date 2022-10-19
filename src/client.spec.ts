418c60fea868b90d48570d1123972fdbdf98569a
Elton Mesquita <callmeelton@gmail.com>
2022-10-19 17:59:17 +0100

    Add initital tests for client

---
+++ src/client.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from 'vitest'
+
+import { makeClient } from './client'
+import { modelsListMock } from '../mocks/fixtures'
+
+describe('API: address endpoint', () => {
+  it('should return the proper model object', async () => {
+    const Client = makeClient({ workspace: 'test-workspace' })
+    const models = await Client.models.list()
+
+    expect(models).toEqual(modelsListMock)
+  })
+
+  it('should throw error when no workspace is specified', async () => {
+    const Client = makeClient()
+
+    expect(Client.getBaseUrl).toThrowError(
+      'No workspace defined. Use the `configure()` method on the Starlight client to define one.'
+    )
+  })
+})
